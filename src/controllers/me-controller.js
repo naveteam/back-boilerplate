a382617948ea5cd9d24708c96d6a5727e8fe53be
Marcos Camargo <marcos.camargo@nave.rs>
2020-01-25 10:08:56 -0300

    refactor(controllers): new approach to controllers coding

--- src/controllers/me-controller.js
+++ src/controllers/me-controller.js
@@ -1,7 +1,7 @@
 import User from 'models/User'
 
-const MeController = {
-  show: ctx => new User({ id: ctx.state.user.id }).fetch()
-}
+export const show = ctx => new User({ id: ctx.state.user.id }).fetch()
 
-export default MeController
+export default {
+  show
+}
