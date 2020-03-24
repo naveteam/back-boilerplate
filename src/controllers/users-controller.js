71d596f6e5d57dac409e1f35b8554231db27e404
Marcos S. Camargo <marcos.camargo@nave.rs>
2020-03-24 09:23:56 -0300

    refactor(controllers): new approach to controllers coding (#14)

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -2,61 +2,63 @@ import User from 'models/User'
 import bcrypt from 'bcryptjs'
 import { Unauthorized, encryptPassword, generateJWTToken } from '../helpers'
 
-const UsersController = {
-  login: async ctx => {
-    const { body } = ctx.request
+export const login = async ctx => {
+  const { body } = ctx.request
 
-    const user = await new User({ email: body.email }).fetch().catch(() => {
-      throw Unauthorized('Unauthorized, User not found')
-    })
+  const user = await new User({ email: body.email }).fetch().catch(() => {
+    throw Unauthorized('Unauthorized, User not found')
+  })
 
-    const isValid = await bcrypt.compare(
-      body.password,
-      user.attributes.password
-    )
+  const isValid = await bcrypt.compare(body.password, user.attributes.password)
 
-    if (!isValid) {
-      throw Unauthorized('Unauthorized, password is invalid')
-    }
+  if (!isValid) {
+    throw Unauthorized('Unauthorized, password is invalid')
+  }
 
-    const parsedUser = user.toJSON()
+  const parsedUser = user.toJSON()
 
-    return {
-      ...parsedUser,
-      token: generateJWTToken({ id: parsedUser.id, role: parsedUser.role })
-    }
-  },
+  return {
+    ...parsedUser,
+    token: generateJWTToken({ id: parsedUser.id, role: parsedUser.role })
+  }
+}
 
-  index: () => new User().fetchAll(),
+export const index = () => new User().fetchAll()
 
-  show: ctx => new User({ id: ctx.params.id }).fetch(),
+export const show = ctx => new User({ id: ctx.params.id }).fetch()
 
-  create: async ctx => {
-    const { body } = ctx.request
+export const create = async ctx => {
+  const { body } = ctx.request
 
-    return new User({
+  return new User({
+    name: body.name,
+    email: body.email,
+    password: await encryptPassword(body.password),
+    role: body.role
+  }).save()
+}
+
+export const update = async ctx => {
+  const { body } = ctx.request
+
+  return new User({ id: ctx.params.id }).save(
+    {
       name: body.name,
       email: body.email,
       password: await encryptPassword(body.password),
       role: body.role
-    }).save()
-  },
-
-  update: async ctx => {
-    const { body } = ctx.request
-
-    return new User({ id: ctx.params.id }).save(
-      {
-        name: body.name,
-        email: body.email,
-        password: await encryptPassword(body.password),
-        role: body.role
-      },
-      { method: 'update' }
-    )
-  },
-
-  destroy: ctx => new User({ id: ctx.params.id }).destroy()
+    },
+    { method: 'update' }
+  )
 }
 
-export default UsersController
+export const destroy = ctx => new User({ id: ctx.params.id }).destroy()
+
+export default {
+  login,
+  index,
+  show,
+  create,
+  update,
+  destroy
+}
