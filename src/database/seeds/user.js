48bddacfcf352a58b2c14b4279dfe2ffb3ed5e9e
Bruno Braga <bruno.braga@nave.rs>
2020-08-24 20:33:38 -0300

    feat(database): let database create uuid from default pgcrypto function

--- src/database/seeds/user.js
+++ src/database/seeds/user.js
@@ -1,12 +1,9 @@
-import { v4 } from 'uuid'
-
 import { encryptPassword } from '../../helpers/password'
 
 export const seed = async knex => {
   await knex('users').del()
   await knex('users').insert([
     {
-      id: v4(),
       name: 'Nave Team',
       email: 'tech@nave.rs',
       password: await encryptPassword('teste1'),
