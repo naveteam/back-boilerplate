fd3d5a9ed5e2c66802e230348622a160f63e5931
Ederson Luís <edergp2009@gmail.com>
2021-02-03 10:05:43 -0300

    refactor(refactor): refactor name logger

--- src/utils/cloudwatch.js
+++ src/utils/cloudwatch.js
@@ -1,9 +1,9 @@
-import { NODE_ENV } from 'config'
+import { NODE_ENV, LOGGER_NAME } from 'config'
 
-export const LOGGER_GROUP = `banco-abc-${NODE_ENV}`
+export const LOGGER_GROUP = `${LOGGER_NAME}-${NODE_ENV}`
 
-export const LOGGER_STREAM_GENERAL = `banco-abc-${NODE_ENV}-stream`
+export const LOGGER_STREAM_GENERAL = `${LOGGER_NAME}-${NODE_ENV}-stream`
 
-export const LOGGER_JOB_FAILED = `banco-abc-${NODE_ENV}-job-failed`
+export const LOGGER_JOB_FAILED = `${LOGGER_NAME}-${NODE_ENV}-job-failed`
 
-export const LOGGER_JOB_ERROR = `banco-abc-${NODE_ENV}-job-error`
+export const LOGGER_JOB_ERROR = `${LOGGER_NAME}-${NODE_ENV}-job-error`
