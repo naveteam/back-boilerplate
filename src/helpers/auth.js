7120bb6fa31fad7f2daf6e526704906633a655f3
guilhermerods <rodsguilherme@gmail.com>
2021-02-18 10:10:46 -0300

    feat: add helper with pandora auth

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcryptjs'
 import { OAuth2 } from '@naveteam/pandora-backend'
 
 import { JWT_SECRET, EXPIRE_TIME } from '../config'
 
-export const generateOAuthToken = dataToEncrypt =>
+export const generateTokens = dataToEncrypt =>
   OAuth2.generateTokens(dataToEncrypt)
+
+export const refreshOAuthToken = body => OAuth2.refreshAccessToken(body)
 
 export const generateJWTToken = tokenData =>
   jwt.sign(tokenData, JWT_SECRET, { expiresIn: EXPIRE_TIME })
--- src/helpers/password.js
+++ src/helpers/password.js
@@ -1,2 +1,4 @@
+import bcrypt from 'bcryptjs'
+
 export const encryptPassword = (password, length = 10) =>
   bcrypt.hash(password, length)
