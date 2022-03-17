1f3232779c3ae94f2c96498c0255c9e8a2e088b1
Cleberson Saller <contato@csaller.com>
2022-03-17 09:42:33 -0300

    feat: added NODE_ENV to allow unencoded variables on local environment

--- src/helpers/base64decoder.js
+++ src/helpers/base64decoder.js
@@ -1,4 +1,8 @@
+import { NODE_ENV } from 'config'
+
 export const base64Decoder = encoded => {
+  if (NODE_ENV === 'local') return encoded
+  
   const buffer = new Buffer.from(encoded, 'base64')
   return buffer.toString('utf-8')
 }
